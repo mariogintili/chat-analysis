ace6bfb52076d75c497ebe68a6a5cd2063738ccf
mariogintili <mariojgintili@gmail.com>
2016-08-18 09:31:11 +0100

    snake_case all variable names to be compliant with Googles style guide

--- notebooks/base.py
+++ notebooks/base.py
@@ -15,18 +15,25 @@ def conversations_abandoned_chart():
   print 'Total number of 24/7 chats that were open are'
   print dataframe.shape[0]
   print "Out that number, we can see that only a proportion of them actually started"
-  conversationsDict = { 'started': 0, 'failed': 0 }
+  conversations_dict = { 'started': 0, 'failed': 0 }
 
   for convo in dataframe['conversation']:
       if len(json.loads(convo)) > 0:
-          conversationsDict['started'] = conversationsDict['started'] + 1
+          conversations_dict['started'] = conversations_dict['started'] + 1
       else:
-          conversationsDict['failed'] = conversationsDict['failed'] + 1
+          conversations_dict['failed'] = conversations_dict['failed'] + 1
 
-  conversationHappenedSeries = pandas.Series([conversationsDict['started'], conversationsDict['failed']], index=['started', 'abandoned'], name='24/7 Usage')
-  print conversationHappenedSeries;
-  return conversationHappenedSeries.plot.pie(labels=['Started', 'Abandoned'], colors=['b', 'r'], autopct='%.2f', fontsize=20, figsize=(6, 6))
+  conversation_happened_series = pandas.Series([conversations_dict['started'], conversations_dict['failed']], index=['started', 'abandoned'], name='24/7 Usage')
+  print conversation_happened_series;
+  return conversation_happened_series.plot.pie(labels=['Started', 'Abandoned'], colors=['b', 'r'], autopct='%.2f', fontsize=20, figsize=(6, 6))
 
 
 def conversations_abandoned_by_referral_platform():
   dataframe = build_dataframe('SELECT * from conversations;')
+  failed_conversations = []
+
+  for convo in dataframe['conversation']:
+    conversation_document = json.loads(convo)
+    if len(conversation_document) < 0:
+      failed_conversations.append(conversation_document)
+
